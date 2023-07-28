3724f00aaaa9a2677281d5eb300966ccf65de139
Alexandr Hrynenko <layt.lat@ukr.net>
2023-07-28 11:03:21 +0300

    Update authControllers.js

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -57,7 +57,7 @@ const login = async (req, res) => {
 const logout = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: null });
-  res.status(204).json({});
+  res.status(204).send();
 };
 
 const getCurrentUser = async (req, res) => {
